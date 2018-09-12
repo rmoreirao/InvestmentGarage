c92e0e56303fd7d8cf0d29c3ff18e8a4adb4fade
Rodrigo <rmoreirao@gmail.com>
2018-09-12 22:56:25 +0200

    Update...

--- manager/quotes_manager.py
+++ manager/quotes_manager.py
@@ -11,6 +11,7 @@ from pathlib import Path,WindowsPath
 # data[['4. close']].plot()
 # plt.title('Intraday Times Series for the MSFT stock (1 min)')
 # plt.show()
+from util.cache_util import memoize
 
 COL_DATE = 'date'
 COL_OPEN = '1. open'
@@ -21,19 +22,18 @@ COL_ADJ_CLOSE = '5. adjusted close'
 COL_VOL = '6. volume'
 COL_DIV = '7. dividend amount'
 COL_SPLIT = '8. split coefficient'
+COL_SYMBOL = 'symbol'
 
-_cache_folder = None
+def __get_quotes_cache_folder():
+    curr_path = Path.cwd()
+    return next(path for path in curr_path.parents if path.name =='InvestmentGarage')
+
+# _get_quotes_cache_folder = memoize(__get_quotes_cache_folder)
 
 def get_daily_quotes(start_date: datetime, end_date: datetime, symbol:str):
-    if not _cache_folder:
-        curr_path_parts = list(Path.cwd().parts)
-        for i in reversed(range(len(curr_path_parts))):
-            if curr_path_parts[i] != 'InvestimentGarage':
-                curr_path_parts.pop(i)
-            else:
-                break
-        _cache_folder = Path(curr_path_parts)
-    file_path =  '../quotes_cache/' + symbol + '.pkl'
+    quotes_cache_path:Path = __get_quotes_cache_folder()
+    quotes_cache_path = quotes_cache_path / 'quotes_cache' / (symbol + '.pkl')
+    file_path =  quotes_cache_path.as_posix()
     df: pd.DataFrame
     if Path(file_path).is_file():
         df = pd.read_pickle(file_path)
@@ -44,6 +44,13 @@ def get_daily_quotes(start_date: datetime, end_date: datetime, symbol:str):
             df.to_pickle(file_path)
         except:
             print("Error trying to save pickle quote: " + file_path)
+
     return df.ix[start_date.strftime('%Y-%m-%d'):end_date.strftime('%Y-%m-%d')]
 
+def test_get_daily_quotes():
+    start_date = datetime(2015, 7, 1)
+    end_date = datetime(2018, 8, 15)
+    symbol = 'AAPL'
+    get_daily_quotes(start_date,end_date,symbol)
 
+test_get_daily_quotes()
